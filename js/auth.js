ffbdb82581d23bb6cb8976e1b08d9920ef88369b
sanjuanteam2019-svg <sanjuanteam2019@gmail.com>
2026-07-28 13:20:44 +0800

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,6 +1,25 @@
+if (localStorage.getItem("loggedIn") !== "true") {
+    window.location.href = "index.html";
+}
+
 <li>
     <a href="#" onclick="logout(); return false;"
        style="color:white;text-decoration:none;">
        🚪 Logout
     </a>
 </li>
+
+const logoutBtn = document.getElementById("logoutBtn");
+
+if (logoutBtn) {
+
+    logoutBtn.addEventListener("click", () => {
+
+        localStorage.removeItem("loggedIn");
+        localStorage.removeItem("username");
+
+        window.location.href = "index.html";
+
+    });
+
+}
