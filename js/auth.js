e9ec4af353d23ef03f11d83832e6fd4c7217bfd4
sanjuanteam2019-svg <sanjuanteam2019@gmail.com>
2026-07-29 22:27:30 +0800

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,14 +1,38 @@
-// Check if a user is logged in
-const currentUser = localStorage.getItem("currentUser");
+// =========================
+// Authentication Check
+// =========================
 
-if (!currentUser) {
+const session = JSON.parse(
+    localStorage.getItem("currentUser")
+);
+
+if (!session) {
+
     window.location.href = "index.html";
+
 }
 
-// Logout button
+// =========================
+// Welcome User
+// =========================
+
+const welcomeUser = document.getElementById("welcomeUser");
+
+if (welcomeUser && session) {
+
+    welcomeUser.textContent =
+        `Welcome, ${session.fullname}`;
+
+}
+
+// =========================
+// Logout
+// =========================
+
 const logoutBtn = document.getElementById("logoutBtn");
 
 if (logoutBtn) {
+
     logoutBtn.addEventListener("click", () => {
 
         localStorage.removeItem("currentUser");
@@ -16,4 +40,5 @@ if (logoutBtn) {
         window.location.href = "index.html";
 
     });
+
 }
