117dfccfc39006093e4684489e2b83a7c21f7c58
sanjuanteam2019-svg <sanjuanteam2019@gmail.com>
2026-08-13 21:44:48 +0800

    Update documents.js

--- js/documents.js
+++ js/documents.js
@@ -499,26 +499,6 @@ function displayDocuments(list) {
                 </td>
 
                 <td>
-                    ${doc.activityId || ""}
-                </td>
-
-                <td>
-                    ${doc.ativityName || ""}
-                </td>
-
-                <td>
-                    ${doc.fileId || ""}
-                </td>
-
-                <td>
-                    ${doc.fileName || ""}
-                </td>
-
-                <td>
-                    ${doc.uploadedBy || ""}
-                </td>
-
-                <td>
                     ${fileCell}
                 </td>
 
